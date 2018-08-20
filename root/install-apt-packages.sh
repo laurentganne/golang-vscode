24d494039df64dbf482698c3828d4d9e93f77cad
Laurent Ganne <ganne.laurent@gmail.com>
2018-08-20 14:51:27 +0200

    Added zip installation

--- root/install-apt-packages.sh
+++ root/install-apt-packages.sh
@@ -23,6 +23,7 @@ apt-get install -y --no-install-recommends \
   texlive-fonts-recommended \
   texlive-latex-extra \
   terminator && \
+  zip && \
 curl -o vscode-amd64.deb -L https://go.microsoft.com/fwlink/?LinkID=760868 && \
 dpkg -i vscode-amd64.deb
 
